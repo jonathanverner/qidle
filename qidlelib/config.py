cccd748ffcbb69ca276d0abe9357a321fd89289d
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2013-03-30 11:15:40 +0100

    More work on the config backend

--- qidlelib/config.py
+++ qidlelib/config.py
@@ -20,18 +20,21 @@ class configuration(object):
         return self.__dict__.items()
     
     def __str__(self):
-        return str(self.__dict__)
+        return json.dumps(self.__dict__, indent = 4, encoding = 'utf-8')
     
-    def save_to_file(self):
-        f = codecs.EncodedFile(open(expanduser("~/.qidlerc"),'w'), 'utf-8')
+    def save(self, fpath = expanduser("~/.qidlerc")):
+        f = codecs.EncodedFile(open(fpath,'w'), 'utf-8')
         json.dump(self.__dict__,f,indent=4, encoding='utf-8', ensure_ascii=False)
         f.close()
     
-    def load_from_file(self):
+    def load(self, fpath = expanduser("~/.qidlerc")):
         try:
-            f = codecs.EncodedFile(open(expanduser("~/.qidlerc"), 'r'),'utf-8')
+            f = codecs.EncodedFile(open(fpath, 'r'),'utf-8')
             self.__dict__ = json.load(f)
             return True
         except:
             return False
         
+
+config = configuration()
+config.load()
